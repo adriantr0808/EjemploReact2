a45691273ad8aff8c016e5c84e7c671e697dba2b
adriantr0808 <adriantr.gt@gmail.com>
2021-10-19 09:42:40 +0200

    update

--- src/components/SumarNumeros/SumarNumeros.js
+++ src/components/SumarNumeros/SumarNumeros.js
@@ -1,8 +1,7 @@
-import Img from '../../assets/2.jpg' //Importamos la imagen 
+import Img from "../../assets/2.jpg"; //Importamos la imagen
 function Metodos() {
-  const sumarNumeros = () => { //Metodo en el que voy a sumar dos numeros
-    var numero1 = 5;
-    var numero2 = 4;
+  const sumarNumeros = (numero1, numero2) => {
+    //Metodo en el que voy a sumar dos numeros introducidos como parametro
 
     var resultado = "";
     resultado = numero1 + numero2;
@@ -11,19 +10,23 @@ function Metodos() {
 
   return (
     <div>
-      <h1 style={{margin: '1em'}}> Ejemplo 1 Sumar numeros e imagen </h1>
+      <h1 style={{ margin: "1em" }}> Ejemplo 1 Sumar numeros e imagen </h1>
       <button
         style={{
           padding: "2px",
           margin: "1em",
         }}
-        onClick={() => sumarNumeros()}
+        onClick={() => sumarNumeros(5, 5)} //Llamo a la funcion con los numeros introducidos como parametro
       >
         Pulsar para sumar
       </button>
       <div>
-        <h3 style={{margin: '1em'}}>Imagen</h3>
-        <img src={Img} alt={'imagenPrueba'} style={{width: '500px', height: '350px', margin: '1em'}}></img>
+        <h3 style={{ margin: "1em" }}>Imagen</h3>
+        <img
+          src={Img}
+          alt={"imagenPrueba"}
+          style={{ width: "500px", height: "350px", margin: "1em" }}
+        ></img>
       </div>
     </div>
   );
